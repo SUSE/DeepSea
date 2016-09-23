c865b65eed3b3e4940f30c54d37687e13e5450e0
Eric Jackson <ejackson@suse.com>
2016-09-23 17:52:17 -0400

    Update version
    Signed-off-by: Eric Jackson <ejackson@suse.com>

--- srv/salt/_modules/keyring.py
+++ srv/salt/_modules/keyring.py
@@ -5,9 +5,16 @@ import struct
 import base64
 import time
 
-def secret():
+def secret(filename):
     """
     """
+    if os.path.exists(filename):
+        with open(filename, 'r') as keyring:
+            for line in keyring:
+                if 'key' in line and ' = ' in line:
+                    key = line.split(' = ')[1].strip()
+                    return key
+
     key = os.urandom(16)
     header = struct.pack('<hiih',1,int(time.time()),0,len(key))
     return base64.b64encode(header + key)
