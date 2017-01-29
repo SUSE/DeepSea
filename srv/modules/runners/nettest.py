7d1f8adf6af5af6f0ac5363922442c0912485a79
Alex Lau (AvengerMoJo) <alau@suse.com>
2017-01-29 23:35:45 +0800

    Add ping runner and update sleep async in mod
    Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>

--- srv/modules/runners/nettest.py
+++ srv/modules/runners/nettest.py
@@ -1,149 +1,19 @@
 #!/usr/bin/python
 
 import salt.client
+import salt.config
+import salt.loader
+
 import select
 import logging
+import socket
 
 log = logging.getLogger(__name__)
 
-
-def _get_minion_ipv4_dict(**kwargs):
+def ping():
     """
-    Return dict of minion, ipv4 list
-
-    The minion match the search criteria, and the ip addresses are from minions
-    that also match the search criteria, but returned touples are never for
-    minions with thier own ip addresses.
+    Return True if any node is not being connected. 
     """
-    node_list = select.minions(**dict(kwargs, host=False))
-    local_client = salt.client.LocalClient()
-    node_ip_map = {}
-    for node in node_list:
-        ipv4_list = set()
-        address_list = local_client.cmd(node, 'grains.get', ['ipv4'])
-        if not node in address_list.keys():
-            log.error("Failed getting ipv4 address from node:{node}".format(node=node))
-            log.warning("Skipping all tests for node:{node}".format(node=node))
-            continue
-        for addr in address_list[node]:
-            if addr == '127.0.0.1':
-                continue
-            ipv4_list.add(addr)
-        node_ip_map[node] = ipv4_list
-    return node_ip_map
+    local = salt.client.LocalClient()
+    return local.cmd(socket.gethostname(), 'nettest.ping_all_minions', ['filter_type=fail'])
 
-
-def minion_link_ipv4(**kwargs):
-    """
-    Return list of (minion, ipv4) touples
-
-    The minion match the search criteria, and the ip addresses are from minions
-    that also match the search criteria, but returned touples are never for
-    minions with thier own ip addresses.
-    """
-    node_ip_map = _get_minion_ipv4_dict(**kwargs)
-    output = []
-    for node_src in node_ip_map.keys():
-        for node_dest in node_ip_map.keys():
-            if node_dest == node_src:
-                continue
-            for ipv4 in node_ip_map[node_dest]:
-                output.append((node_src, ipv4))
-    return output
-
-
-def minion_link_ipv4_parallel(**kwargs):
-    """
-    Return list of ([minion], ipv4) touples
-
-    The minion match the search criteria, and the ip addresses are from minions
-    that also match the search criteria, but returned touples are never for
-    minions with thier own ip addresses.
-    """
-    node_ip_map = _get_minion_ipv4_dict(**kwargs)
-    output = []
-    node_set = set(node_ip_map.keys())
-    for node_src in node_ip_map.keys():
-        node_src_set = node_set.difference(set([node_src]))
-        for ipv4 in node_ip_map[node_src]:
-            output.append((','.join(node_src_set), ipv4))
-    return output
-
-
-def _minion_link_ipv4_parallel_pair(**kwargs):
-    """
-    Return list of [(minion, ipv4), ...] touples where no minion is listed twice in one result.
-
-    The minion match the search criteria, and the ip addresses are from minions
-    that also match the search criteria.
-
-    Returned touple lists never include the same minion.
-    Returned touples are never for minions with thier own ip addresses.
-    Returned list of tuples never contain the the same minion or ip address in the list.
-
-    Use case is network tests such as iperf.
-    """
-    node_ip_map = _get_minion_ipv4_dict(**kwargs)
-    # Get a map to find node from ip
-    ip_node_map = {}
-    for node_id in node_ip_map.keys():
-        for ipv4 in node_ip_map[node_id]:
-            if ipv4 in ip_node_map.keys():
-                msg = "Duplicate IP address on {node1} and {node2}".format(
-                    node1 = node_id,
-                    node2 = ip_node_map[ipv4]
-                    )
-                log.error(msg)
-                continue
-            ip_node_map[ipv4] = node_id
-    tests_needed = set()
-    for node_src in node_ip_map.keys():
-        for node_dest in node_ip_map.keys():
-            if node_dest == node_src:
-                continue
-            for ipv4 in node_ip_map[node_dest]:
-                tests_needed.add((node_src, ipv4))
-    output = []
-    while len(tests_needed) > 0:
-        busy_nodes = set()
-        selected_tests = set()
-        # now run through each test
-        for test in tests_needed:
-            node1 = test[0]
-            if node1 in busy_nodes:
-                continue
-            ipv4 = test[1]
-            node2 = ip_node_map[ipv4]
-            if node2 in busy_nodes:
-                continue
-            busy_nodes.add(node1)
-            busy_nodes.add(node2)
-            selected_tests.add(test)
-        output.append(list(selected_tests))
-        tests_needed = tests_needed.difference(selected_tests)
-    return output
-
-
-def minion_link_ipv4_parallel_pair(**kwargs):
-    """
-    Return list of [(minionlist, ipv4list)] touples where no minion is listed twice in one result.
-    minionlist and ipv4list are CSV items to make passing as comand line parameters easier.
-
-    The minion match the search criteria, and the ip addresses are from minions
-    that also match the search criteria.
-
-    Returned touple lists never include the same minion.
-    Returned touples are never for minions with thier own ip addresses.
-
-    Use case is network tests such as iperf.
-    """
-    output = []
-    list_tests_run = _minion_link_ipv4_parallel_pair(**kwargs)
-    for test_run in list_tests_run:
-        minion_list = []
-        ipv4_list = []
-        for minion, ipv4 in test_run:
-            minion_list.append(minion)
-            ipv4_list.append(ipv4)
-        output.append((','.join(minion_list),','.join(ipv4_list)))
-    return output
--- srv/salt/_modules/nettest.py
+++ srv/salt/_modules/nettest.py
@@ -14,6 +14,28 @@ import time
 
 log = logging.getLogger(__name__)
 
+'''
+   nettest is the module to test network configuration before state 0 
+   to understand what node may be not connecting to the other correctly
+   
+   Ping is a simple test to check if point to point nodes are connected
+   All ping function reply with a default filter type 'full' which is 
+   the default log reply from ping command itself. 
+   
+   filter_type:
+    'full' - default ping log 
+    'success' - if 4 ping all successfully reply
+    'fail' - if 4 ping has any fail to reply
+    'avg' - average reply time 
+   
+   CLI Example:
+   .. code-block:: bash
+   sudo salt 'node' nettest.ping <hostname>|<ip> <filter_type=[full,success,fail,avg]>
+   sudo salt 'salt-master' nettest.multi_ping <ping_from_hostname>|<ip> 
+       <ping_to_hostname>|<ip>... <filter_type=[full,success,fail,avg]>
+   sudo salt 'salt-master' nettest.ping_all_minions <filter_type=[full,success,fail,avg]>
+
+'''
 
 def _get_all_minions():
     '''
@@ -23,6 +45,7 @@ def _get_all_minions():
     pki_dir = pki_dir.replace('minion', 'master')
     minion_path = os.path.join(pki_dir, salt_key.Key.ACC)
     minion_list = []
+    log.debug('internal function _get_all_minion: check path {}'.format(minion_path))
     try:
         for keys in salt_utils.isorted(os.listdir(minion_path)):
             if not keys.startswith('.'):
@@ -30,7 +53,7 @@ def _get_all_minions():
                     minion_list.append(keys)
     except (OSError, IOError):
     # if key dir is not created skip
-        minion_list = "No Minions"
+        log.debug('key dir is not create')
     return minion_list
 
 def _get_master():
@@ -38,25 +61,38 @@ def _get_master():
     internal function to get salt master 
     '''
     master_host = __salt__['pillar.get']('master_minion')
+    log.debug('internal function _get_master: {}'.format(master_host))
     if not master_host or master_host == '_REPLACE_ME_':
         master_host = False
     return master_host
 
-def _ping_log_success( ping_log ):
-    m = re.match(r'.*4 received, 0% packet loss', ping_log, re.DOTALL)
-    if m:
-        return True
-    else:
+def _is_master():
+    '''
+    internal function to make sure called node is salt-master 
+    '''
+    master_host = _get_master()
+    localhost = socket.gethostname()
+    log.debug('internal function _is_master: localhost {}'.format(localhost))
+    if not master_host:
         return False
-
-def _ping_log_fail( ping_log ):
-    m = re.match(r'.*4 received, 0% packet loss', ping_log, re.DOTALL)
-    if m:
+    else:
+        try:
+            master_ip = socket.gethostbyname(master_host)
+            local_ip = socket.gethostbyname(localhost)
+        except socket.gaierror:
+            log.exception('Error hostname not find: master/{}, localhost hostname/{}'.format(master_host,localhost))
+            print('Error hostname not find: master/{}, localhost hostname/{}'.format(master_host,localhost))
+            print(socket.gaierror)
+            return False
+    if local_ip != master_ip:
         return False
     else:
         return True
 
 def _ping_log_avg( ping_log ):
+    '''
+    internal filter function to grab the average reply speed
+    '''
     if _ping_log_success(ping_log):
         m = re.match(r'.*rtt min/avg/max/mdev = \d+\.?\d+/(\d+\.?\d+)/', ping_log, re.DOTALL)
         if m:
@@ -66,41 +102,17 @@ def _ping_log_avg( ping_log ):
     else:
         return False
 
-def _is_master():
-    master_host = _get_master()
-    localhost = socket.gethostname()
-    if not master_host:
-        return False
-    else:
-        try:
-            master_ip = socket.gethostbyname(master_host)
-            local_ip = socket.gethostbyname(localhost)
-        except socket.gaierror:
-                print('Error hostname not find: master/{}, localhost hostname/{}'.format(master_host,localhost))
-                print(socket.gaierror)
-                return False
-    if local_ip != master_ip:
+def _ping_log_fail( ping_log ):
+    '''
+    internal filter function to if there is any package lost 
+    '''
+    m = re.match(r'.*4 received, 0% packet loss', ping_log, re.DOTALL)
+    if m:
         return False
     else:
         return True
 
-def ping( node, filter_type='full'):
-    '''
-    Ping a client node 4 times and return result
-
-    CLI Example:
-    .. code-block:: bash
-    salt 'node' nettest.ping <hostname>|<ip>
-    '''
-    if not node:
-        node = socket.gethostname()
-    ping_out = __salt__['cmd.run']('/usr/bin/ping -c 4 ' + node , output_loglevel='debug')
-    if( ping_log_filter(ping_out, filter_type) ):
-        return node + ': ' + str(ping_log_filter(ping_out, filter_type))
-    else:
-        return False
-
-def ping_log_filter(ping_log, filter_type='full'): 
+def _ping_log_filter(ping_log, filter_type='full'): 
     '''
     internal function to filter log result base on filter type
     'full' - default ping log 
@@ -115,6 +127,16 @@ def ping_log_filter(ping_log, filter_type='full'):
     } 
     func = filtering.get(filter_type, lambda: ping_log)
     return func()
+
+def _ping_log_success( ping_log ):
+    '''
+    internal filter function to if all package get received 
+    '''
+    m = re.match(r'.*4 received, 0% packet loss', ping_log, re.DOTALL)
+    if m:
+        return True
+    else:
+        return False
 
 def multi_ping( ping_from, *nodes, **kwargs):
     '''
@@ -132,6 +154,9 @@ def multi_ping( ping_from, *nodes, **kwargs):
     localhost = socket.gethostname()
     master_host = _get_master()
 
+    log.debug('multi_ping: ping_from {}'.format(ping_from))
+    log.debug('node_list {}'.format(nodes))
+    log.debug('filter_type {}'.format(filter_type))
     if len(nodes) < 1:
         return "\n Multi_ping need a client nodes list\n"
     ping_jid = []
@@ -139,11 +164,15 @@ def multi_ping( ping_from, *nodes, **kwargs):
     ping_log = []
     for i, node in enumerate( nodes ):
         ping_jid.append( __salt__['cmd.run']('/usr/bin/salt --async ' + ping_from + ' nettest.ping ' + node + ' filter_type=' + filter_type, output_loglevel='debug'))
-    time.sleep(3)
-    for log in ping_jid:
-        m = re.match(r'Executed command with job ID: (\d+)', log,  re.DOTALL)
+    #time.sleep(3)
+    for jid_log in ping_jid:
+        log.debug('jid_log {}'.format(jid_log))
+        m = re.match(r'Executed command with job ID: (\d+)', jid_log,  re.DOTALL)
         if m :
-            m_log = __salt__['cmd.run']('/usr/bin/salt-run jobs.lookup_jid ' + m.group(1), output_loglevel='debug')
+            m_log = {}
+            while not m_log:
+                time.sleep(1) 
+                m_log = __salt__['cmd.run']('/usr/bin/salt-run jobs.lookup_jid ' + m.group(1), output_loglevel='debug')
             not_fail = re.match(r'.*False', m_log, re.DOTALL)
             if not not_fail: 
                 temp_log.append( m_log )
@@ -152,6 +181,25 @@ def multi_ping( ping_from, *nodes, **kwargs):
     for line in temp_log:
         ping_log.append(re.sub(r'(' + ping_from + ':\n\s)' , '', line))
     return ping_log
+
+def ping( node, filter_type='full'):
+    '''
+    Ping a client node 4 times and return result
+
+    CLI Example:
+    .. code-block:: bash
+    salt 'node' nettest.ping <hostname>|<ip>
+    '''
+    if not node:
+        return "\n ping need a client node\n"
+    log.debug('ping: to {}'.format(node))
+    log.debug('filter_type {}'.format(filter_type))
+
+    ping_out = __salt__['cmd.run']('/usr/bin/ping -c 4 ' + node , output_loglevel='debug')
+    if( _ping_log_filter(ping_out, filter_type) ):
+        return node + ': ' + str(_ping_log_filter(ping_out, filter_type))
+    else:
+        return False
 
 def ping_all_minions(filter_type='full'):
     '''
@@ -166,6 +214,9 @@ def ping_all_minions(filter_type='full'):
         return "This function need to run in the salt master!\n"
     master_host = _get_master()
     minion_list = _get_all_minions()
+    log.debug('ping_all_minions: master {}'.format(master_host))
+    log.debug('minion_list {}'.format(minion_list))
+    log.debug('filter_type {}'.format(filter_type))
     ping_jid = []
     temp_log = []
     ping_log = []
@@ -174,19 +225,21 @@ def ping_all_minions(filter_type='full'):
         call_list.remove(minion)
         mp_jid = __salt__['cmd.run']('/usr/bin/salt --async ' + master_host + '  nettest.multi_ping ' + minion + ' ' + ' '.join(call_list) + ' filter_type=' + filter_type, output_loglevel='debug')
         ping_jid.append({'jid':mp_jid, 'ping_from':minion})
-    time.sleep(len(minion_list) + 4)
-    for log in ping_jid:
-        m = re.match(r'Executed command with job ID: (\d+)', log['jid'],  re.DOTALL)
+    #time.sleep(len(minion_list) + 4)
+    for jid_log in ping_jid:
+        log.debug('jid_log {}'.format(jid_log['jid']))
+        m = re.match(r'Executed command with job ID: (\d+)', jid_log['jid'],  re.DOTALL)
         if m:
-            m_log = __salt__['cmd.run']('/usr/bin/salt-run jobs.lookup_jid ' + m.group(1), output_loglevel='debug')
+            m_log = {}
+            while not m_log:
+                time.sleep(1) 
+                m_log = __salt__['cmd.run']('/usr/bin/salt-run jobs.lookup_jid ' + m.group(1), output_loglevel='debug')
             not_fail = re.match(r'.*False', m_log, re.DOTALL)
             if not not_fail:
                 temp_log.append(m_log)
-        else:
-            temp_log.append("Not done " + log['jid'])
     if len(temp_log) == 0 and filter_type == 'fail':
         return 'Nothing fail!\n'
-    for i, log in enumerate(temp_log):
-        ping_log.append(re.sub(r'(' + master_host + ':\n)' , ping_jid[i]['ping_from'] + ':\n' , log))
+    for i, t_log in enumerate(temp_log):
+        ping_log.append(re.sub(r'(' + master_host + ':\n)' , ping_jid[i]['ping_from'] + ':\n' , t_log))
     return ping_log
 
