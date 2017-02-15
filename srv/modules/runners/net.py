33411c0074ca39c36b17bbfa05d5e4d77f6cc211
Alex Lau (AvengerMoJo) <alau@suse.com>
2017-02-15 19:56:57 +0800

    sorting by the function name
    Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>
    
    Add iperf client and server command into module
    
    Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>
    
    Add iperf kill and filter for speed per sec
    
    Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>
    
    Work in progress push and debug later
    
    Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>
    
    Update iperf output to give total bandwidth
    
    Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>
    
    Change the unit to Megabits instead of Byte
    
    Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>
    
    Remove debug print out and add excluded iperf func
    
    Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>
    
    Fix the loop with xrange to get all clients called
    
    Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>

--- srv/modules/runners/net.py
+++ srv/modules/runners/net.py
@@ -1,12 +1,131 @@
 #!/usr/bin/python
 
 import logging
+import multiprocessing
+import operator
 import re
 import salt.client
+import time
+
+
 
 from netaddr import IPNetwork, IPAddress
 
 log = logging.getLogger(__name__)
+
+def get_cpu_count(server):
+    local = salt.client.LocalClient()
+    #node, cpu_core = (local.cmd("S@"+server, 'grains.item',  ['num_cpus'], expr_form="compound")).popitem()
+    result = local.cmd("S@"+server + " or " + server , 'grains.item',  ['num_cpus'], expr_form="compound")
+    cpu_core = result.values()[0]['num_cpus']
+    return cpu_core
+
+def iperf(cluster = None, exclude = None, **kwargs):
+    """
+    iperf server created from the each minions and then clients are created 
+    base on the server's cpu count and request that number of other minions
+    as client to hit the server and report the total bendwidth. 
+
+    TODO: Convert commented out print statements to log.debug
+
+    CLI Example: (Before DeepSea with a cluster configuration)
+    .. code-block:: bash
+        sudo salt-run net.iperf
+
+    or you can run it with exclude
+    .. code-block:: bash
+        sudo salt-run net.iperf exclude="E@host*,host-osd-name*,192.168.1.1"
+
+    (After DeepSea with a cluster configuration)
+    .. code-block:: bash
+        sudo salt-run net.iperf cluster=ceph
+        sudo salt-run net.iperf ceph
+
+    """
+    exclude_string = exclude_iplist = None
+    if exclude:
+        exclude_string, exclude_iplist = _exclude_filter(exclude)
+
+    addresses = []
+    local = salt.client.LocalClient()
+    if cluster:
+        search = "I@cluster:{}".format(cluster)
+
+        if exclude_string:
+            search += " and not ( " + exclude_string + " )"
+            log.debug( "iperf: search {} ".format(search))
+
+        networks = local.cmd(search , 'pillar.item', [ 'cluster_network', 'public_network' ], expr_form="compound")
+        total = local.cmd(search , 'grains.get', [ 'ipv4' ], expr_form="compound")
+        addresses = []
+        for host in sorted(total.iterkeys()):
+            if 'cluster_network' in networks[host]:
+                addresses.extend(_address(total[host], networks[host]['cluster_network']))
+            if 'public_network' in networks[host]:
+                addresses.extend(_address(total[host], networks[host]['public_network']))
+    else:
+        search = "*"
+        if exclude_string:
+            search += " and not ( " + exclude_string + " )"
+            log.debug( "ping: search {} ".format(search))
+        addresses = local.cmd(search , 'grains.get', [ 'ipv4' ], expr_form="compound")
+        addresses = _flatten(addresses.values())
+        # Lazy loopback removal - use ipaddress when adding IPv6
+        try:
+            if addresses:
+                addresses.remove('127.0.0.1')
+            if exclude_iplist:
+                for ex_ip in exclude_iplist:
+                    log.debug( "ping: removing {} ip ".format(ex_ip))
+                    addresses.remove(ex_ip)
+        except ValueError:
+            log.debug( "ping: remove {} ip doesn't exist".format(ex_ip))
+            pass
+    #print addresses
+    start_service = []
+    results = []
+    jid = []
+    log.debug( "iperf: minions list {} ".format(addresses) )
+    for server in addresses:
+        cpu_core = get_cpu_count(server)
+        log.debug( "iperf: server {} cpu count {} ".format(server, cpu_core) )
+        for x in range(cpu_core):
+            log.debug( "iperf: create server {} count {} port {} ".format(server, x, 5800+x ) )
+            start_service.append( local.cmd( "S@"+server, 'multi.iperf_server_cmd', [ x, 5800+x ], expr_form="compound"))
+
+    for server in addresses: 
+        cpu_core = get_cpu_count(server)
+        log.debug( "iperf: server {} cpu count {} ".format(server, cpu_core) )
+        clients = list(addresses)
+        clients.remove(server)
+        clients_size = len(clients)
+        for x in xrange(0, cpu_core, clients_size):
+            for y, client in enumerate(clients):
+                log.debug( "iperf: server port num {}, num {} client {} to server {}".format(5800+x+y,x/clients_size, client, server) )
+                if( x+y < cpu_core ):
+    	            jid.append( local.cmd_async("S@"+client, 'multi.iperf', [server, x/clients_size, 5800+x+y ], expr_form="compound"))
+                    log.debug( "iperf: actually called from {} to server {} with cpu {} port {} ".format(client, server, x/clients_size, 5800+x+y) )
+        log.debug( "iperf: Server {} async iperf client count {}".format(server, len(jid)) )
+
+    log.debug( "iperf: All Async iperf client count {}".format(len(jid)) )
+    not_done = True
+    while not_done:
+        not_done = False
+        for job in jid:
+            if not __salt__['jobs.lookup_jid'](job):
+                log.debug( "iperf: job not done {} ".format(job) )
+                time.sleep(1)
+                not_done = True
+    results = []
+    for job in jid:
+            #results.update( __salt__['jobs.lookup_jid'](job) )
+            results.append( __salt__['jobs.lookup_jid'](job) )
+            #results.append( job )
+    
+    # return results
+    return _summarize_iperf( results ) 
+    #return "Done" 
+    #_summarize(len(addresses), results)
 
 def ping(cluster = None, exclude = None, **kwargs):
     """
@@ -210,6 +329,43 @@ def _summarize(total, results):
         print "Failed: \n    {}".format("\n    ".join(failed))
     if errored:
        print "Errored: \n    {}".format("\n    ".join(errored))
+
+def _iperf_result_get_server(result):
+    return result['server']
+
+def _summarize_iperf(results):
+    """
+    iperf summarize the successes, failures and errors across all minions
+    """
+    server_results = {}
+    for result in results:
+        for host in result:
+            #print "Server:\n{}".format(result[host]['server'])
+	    if not result[host]['server'] in server_results:
+	        server_results.update( {result[host]['server']:""} )
+	        #server.update( {result[host]['server'],list()} )
+            if result[host]['succeeded']:
+                #print "success:\n{}".format(result[host]['succeeded']) 
+                #print "filter:\n{}".format(result[host]['filter']) 
+	        server_results[result[host]['server']] += " " + result[host]['filter']
+            elif result[host]['failed']:
+                #print "failed:\n{}".format(result[host]['failed']) 
+		server_results[result[host]['server']] += " Failed to connect from {}".format(host)
+            elif result[host]['errored']:
+                #print "errored :\n{}".format(result[host]['errored']) 
+		server_results[result[host]['server']] += " Error to connect from {}".format(host)
+
+    for key, result in server_results.iteritems():
+        total = 0
+        speed = result.split('Mbits/sec')
+	speed = filter(None, speed)
+        try:
+            for v in speed:
+                total += float(v.strip())
+            server_results[key] = str(total) + " Mbits/sec"
+        except ValueError:
+            continue 
+    return server_results
 
 def _skip_dunder(settings):
     """
--- srv/salt/_modules/multi.py
+++ srv/salt/_modules/multi.py
@@ -4,7 +4,7 @@ import logging
 import multiprocessing.dummy
 import multiprocessing
 import re
-from subprocess import call, Popen, PIPE
+from subprocess import check_call, Popen, PIPE, CalledProcessError
 
 log = logging.getLogger(__name__)
 
@@ -31,26 +31,29 @@ def _all(func, hosts):
     pool = multiprocessing.dummy.Pool(threads)
     for instance in pool.map(func, hosts):
         all_instances.append(instance)
-    
+
     return all_instances
 
-
-def ping_cmd(host):
+def _summarize_iperf(result):
     '''
-    Ping a host with 1 packet and return the result
-
-    CLI Example:
-    .. code-block:: bash
-        sudo salt 'node' multi.ping_cmd <hostname>|<ip>
+    Scan the results and summarize for iperf result
     '''
-    cmd = [ "/usr/bin/ping", "-c1", "-q", "-W1", host ]
-    log.debug('ping_cmd hostname={}'.format(host))
+    host, rc, out, err = result
+    msg = {}
+    msg['server'] = host
+    if rc == 0:
+        msg['succeeded'] = True
+    	msg['speed'] = out
+    	msg['filter'] = re.match(r'.*0.00-10.00.*sec\s(.*Bytes)\s+(.*Mbits/sec)', out, re.DOTALL ).group(2)
+    if rc == 1:
+        msg['succeeded'] = False
+        msg['failed'] = True
+    if rc == 2:
+        msg['succeeded'] = False
+        msg['errored'] = True
+    return msg
 
-    proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    proc.wait()
-    return host, proc.returncode, proc.stdout.read(), proc.stderr.read()
-
-def _summarize(results):
+def _summarize_ping(results):
     '''
     Scan the results and summarize
     '''
@@ -71,18 +74,18 @@ def _summarize(results):
         if rc == 2:
             errored.append(host)
 
-    log.debug('multi._summarize average={}'.format(avg))
+    log.debug('multi._summarize_ping average={}'.format(avg))
 
     if avg:
         avg_sum = sum(i.get('avg') for i in avg) / len(avg)
         if len(avg) > 2:
             for i in avg:
                 if (avg_sum * len(avg) / 2) < i.get('avg') :
-                    log.debug('_summarize: slow host running = {} avg = {}, s'.format( i.get('avg'), avg_sum ))
+                    log.debug('_summarize_ping: slow host running = {} avg = {}, s'.format( i.get('avg'), avg_sum ))
                     slow.append(i.get('host'))
     else:
         avg_sum = 0 
-    
+
     msg = {}
     msg['succeeded'] = len(success)
     if failed:
@@ -94,6 +97,60 @@ def _summarize(results):
     msg['avg'] = avg_sum
     return msg
 
+def iperf(server, cpu, port):
+    '''
+    iperf test to a specific server 
+
+    CLI Example:
+    .. code-block:: bash
+        sudo salt 'node' multi.iperf <hostname>|<ip> <cpu_core> <port>
+    '''
+    log.debug('iperf server ={}'.format(server))
+    return _summarize_iperf( iperf_client_cmd( server, cpu, port ) )
+    #return True
+
+def iperf_client_cmd( server, cpu=0, port=5200 ):
+    '''
+    Use iperf to test minion to server
+        
+    CLI Example:
+    .. code-block:: bash
+    salt 'node' multi.iperf_client_cmd <server_name/ip> cpu=<which_cpu_core default 0> port=<default 5200>
+    '''
+    if not server:
+        return False
+    iperf_cmd = [ "/usr/bin/iperf3", "-fm", "-A"+str(cpu), "-t10", "-c"+server, "-p"+str(port)]
+    log.debug('iperf_client_cmd: cmd {}'.format(iperf_cmd))
+    proc = Popen(iperf_cmd, stdout=PIPE, stderr=PIPE)
+    proc.wait()
+    return server, proc.returncode, proc.stdout.read(), proc.stderr.read()
+
+def iperf_server_cmd( cpu=0, port=5200 ):
+    '''
+    Use iperf to test minion to server
+        
+    CLI Example:
+    .. code-block:: bash
+    salt 'node' multi.iperf_server_cmd <server_name/ip> cpu=<which_cpu_core default 0> port=<default 5200>
+    '''
+    iperf_cmd = [ "/usr/bin/iperf3", "-s", "-D", "-A"+str(cpu),  "-p"+str(port)]
+    log.debug('iperf_server_cmd: cmd {}'.format(iperf_cmd))
+    #with open( "/var/log/iperf_cpu" + str(cpu) + "_port" + str(port) + ".log", "wb") as out, \
+    #open( "/var/log/iperf_cpu" + str(cpu) + "_port" + str(port) + ".err", "wb" ) as err:
+    #proc = Popen(iperf_cmd, stdout=out, stderr=err)
+    proc = Popen(iperf_cmd);
+    # it doesn't report fail so no need to check 
+    return True
+
+def kill_iperf_cmd():
+    '''
+    Clean up all the iperf3 server and clean it. 
+    '''
+    kill_cmd = [ "/usr/bin/killall", "-9", "iperf3"]
+    log.debug('kill_iperf_cmd: cmd {}'.format(kill_cmd))
+    proc = Popen(kill_cmd)
+    return True
+
 def ping(*hosts):
     '''
     Ping a list of hosts and summarize the results
@@ -102,7 +159,35 @@ def ping(*hosts):
     .. code-block:: bash
         sudo salt 'node' multi.ping <hostname>|<ip> <hostname>|<ip>....
     '''
+    # I should be filter all the localhost here? 
     log.debug('ping hostlist={}'.format(list(hosts)))
     results = _all(ping_cmd, list(hosts))
-    return _summarize(results)
+    return _summarize_ping(results)
 
+def ping_cmd(host):
+    '''
+    Ping a host with 1 packet and return the result
+
+    CLI Example:
+    .. code-block:: bash
+        sudo salt 'node' multi.ping_cmd <hostname>|<ip>
+    '''
+    cmd = [ "/usr/bin/ping", "-c1", "-q", "-W1", host ]
+    log.debug('ping_cmd hostname={}'.format(host))
+    proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
+    proc.wait()
+    return host, proc.returncode, proc.stdout.read(), proc.stderr.read()
+
+def prepare_iperf_server():
+    '''
+    Create N server base on the total core number of your cpu count
+
+    CLI Example:
+    .. code-block:: bash
+    salt 'node' multi.prepare_iperf_server 
+
+    '''
+    cpus = multiprocessing.cpu_count()
+    for cpu in range(cpus):
+        iperf_server_cmd( cpu, 5200+cpu )
+    return True
