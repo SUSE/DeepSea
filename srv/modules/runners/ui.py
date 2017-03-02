4c7e904965f0cb229a195e12f88975bdd6bae41d
Eric Jackson <ejackson@suse.com>
2017-03-02 14:54:08 -0500

    Add save, canned examples
    Signed-off-by: Eric Jackson <ejackson@suse.com>

--- srv/modules/runners/ui.py
+++ srv/modules/runners/ui.py
@@ -4,6 +4,7 @@ import logging
 import sys
 import os
 import json
+import yaml
 import salt.client
 import salt.utils.minions
 
@@ -33,6 +34,9 @@ class Iscsi(object):
 	"""
 	"""
 	self.data = {}
+	self.friendly_dumper = yaml.SafeDumper
+	self.friendly_dumper.ignore_aliases = lambda self, data: True
+	self.local = salt.client.LocalClient()
 
     def populate(self):
 	"""
@@ -54,8 +58,7 @@ class Iscsi(object):
 	_stdout = sys.stdout
 	sys.stdout = open(os.devnull, 'w')
 
-	local = salt.client.LocalClient()
-	igws = local.cmd("I@roles:igw", 'grains.get', [ 'ipv4'], expr_form="compound")
+	igws = self.local.cmd("I@roles:igw", 'grains.get', [ 'ipv4'], expr_form="compound")
 	sys.stdout = _stdout
 	if wrapped:
 	    config = []
@@ -101,19 +104,91 @@ class Iscsi(object):
 	Overwrite /srv/salt/ceph/igw/cache/lrbd.conf
 	"""
 	if 'data' in kwargs:
+	    self._set_igw_config(**kwargs)
 	    contents = kwargs['data']
 	    with open(filename, 'w') as conf:
 		conf.write(json.dumps(contents, indent=4) + '\n')
 	else:
 	    log.error("No JSON data passed")
 
+    def _set_igw_config(self, cluster='ceph', **kwargs):
+	"""
+	Add igw_config to cluster.yml
 
+	Comments are removed from cluster.yml
+	"""
+	if 'filename' in kwargs:
+	    filename = kwargs['filename']
+	else:
+	    filename = '/srv/pillar/ceph/stack/{}/cluster.yml'.format(cluster)
+	contents = {}
+	with open(filename, 'r') as yml:
+	    contents = yaml.safe_load(yml)
+	contents['igw_config'] = 'default-ui'
+	with open(filename, 'w') as yml:
+	    yml.write(yaml.dump(contents, Dumper=self.friendly_dumper,
+						  default_flow_style=False))
+	# refresh pillar
+	self.local.cmd("I@roles:master", 'saltutils.pillar_refresh', [ ''], expr_form="compound")
+
+
+    def canned_populate(self, canned):
+	"""
+	Return all canned data
+
+	Note: could add samples from lrbd for config
+	"""
+	self.data['config'] = ''
+	self.data['interfaces'] = self.canned_interfaces(canned)
+	self.data['images'] = self.canned_images(canned)
+
+	return self.data
+
+    def canned_images(self, canned, wrapped=True):
+	"""
+	Return canned example for pools and images
+	"""
+	images = { 1: { 'rbd' : [ 'demo1', 'demo2', 'demo3' ] },
+		   2: { 'car' : [ 'cement' ],
+			'whirl' : [ 'cheese' ],
+			'rbd': [ 'wood', 'writers', 'city' ],
+			'swimming': [ 'cell' ] } }
+	if wrapped:
+	    config = []
+	    for pool in images[canned].keys():
+		for img in images[canned][pool]:
+		    config.append({ 'pool': pool, 'img': img })
+	    return config
+	else:
+	    return images[canned]
+
+    def canned_interfaces(self, canned, wrapped=True):
+	"""
+	Return canned example for nodes and addresses
+	"""
+	interfaces = { 1: { 'igw1' : [ '192.168.0.2', '192.168.1.2', '192.168.2.2' ] },
+		       2: { 'node1': [ '10.0.0.10' ],
+			    'node2': [ '10.0.0.11' ],
+			    'node3': [ '10.0.0.12',
+				       '172.16.31.12',
+				       '192.168.10.112' ],
+			    'node4': [ '1.2.3.4' ] } }
+	if wrapped:
+	    config = []
+	    for node in interfaces[canned].keys():
+		for addr in interfaces[canned][node]:
+		    config.append({ 'node': node, 'addr': addr })
+	    return config
+	else:
+	    return interfaces[canned]
 
 def populate_iscsi(**kwargs):
     """
     Populate the iSCSI view
     """
     i = Iscsi(**kwargs)
+    if 'canned' in kwargs:
+	return i.canned_populate(kwargs['canned'])
     return i.populate()
 
 def save_iscsi(**kwargs):
@@ -129,8 +204,12 @@ def iscsi_config(**kwargs):
 
 def iscsi_interfaces(**kwargs):
     i = Iscsi(**kwargs)
+    if 'canned' in kwargs:
+	return i.canned_interfaces(kwargs['canned'], wrapped=False)
     return i.interfaces(wrapped=False)
 
 def iscsi_images(**kwargs):
     i = Iscsi(**kwargs)
+    if 'canned' in kwargs:
+	return i.canned_images(kwargs['canned'], wrapped=False)
     return i.images(wrapped=False)
