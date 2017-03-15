d1c4d6402ccf881a8dae4413ae7cb08443fbb6bb
Eric Jackson <ejackson@suse.com>
2017-03-15 09:20:34 -0400

    Add cephservices module
    Signed-off-by: Eric Jackson <ejackson@suse.com>

---
+++ srv/salt/_modules/cephservices.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+import salt.client
+import pprint
+import os
+import sys
+import logging
+import time
+import salt.utils
+import salt.utils.master
+
+log = logging.getLogger(__name__)
+
+"""
+The original purpose of this module is to verify that proceeding with an
+upgrade is safe.  All expected services are running.
+
+A secondary purpose is a utility to check the current state of all services.
+
+Note: a runner exists with the same name, but this module is necessary for
+orchestrations to fail properly.  Need naming suggestions. Technically, this
+is checking for processes and not services.
+"""
+
+def check():
+    """
+    Query the status of running processes for each role.  Return False if any 
+    fail. 
+    """
+    processes = { 'mon': [ 'ceph-mon' ],
+                 'storage': [ 'ceph-osd' ],
+                 'mds': [ 'ceph-mds' ],
+                 'igw': [ 'lrbd' ],
+                 'rgw': [ 'radosgw' ],
+                 'ganesha': [ 'ganesha.nfsd', 'rpcbind', 'rpc.statd' ] }
+
+    ret = True
+    if 'roles' in __pillar__:
+        for role in __pillar__['roles']:
+            if role in processes:
+                for process in processes[role]:
+                    result = __salt__['status.pid'](process)
+                    if result == '':
+                        log.error("ERROR: process {} for role {} is not running".format(process, role))
+                        ret = False
+
+    return ret
+
+
+def wait(**kwargs):
+    """
+    Periodically check until all services are up or until the timeout is
+    reached.  Use a backoff for the delay to avoid filling logs.
+
+    Note: state.orch does not obey return codes from runners last time I 
+    checked.  Raising an exception is ugly but does stop the process.
+    """
+    settings = {
+        'timeout': _timeout(),
+        'delay': 3
+    }
+    settings.update(kwargs)
+
+    end_time = time.time() + settings['timeout']
+    current_delay = settings['delay']
+    while end_time > time.time():
+        if check():
+            log.debug("Services are up")
+            return True
+        time.sleep(current_delay)
+        if current_delay < 60:
+            current_delay += settings['delay']
+        else:
+            current_delay = 60
+    log.error("Timeout expired")
+    raise RuntimeError("Timeout expired")
+
+def _timeout():
+    """
+    Assume 15 minutes for physical hardware since some hardware has long 
+    shutdown/reboot times.  Assume 2 minutes for complete virtual environments.
+    """
+    if 'physical' ==  __grains__['virtual']:
+        return 900
+    else:
+        return 120
