f2a8f2083ff7c8eb2c70a28d9965692720729c79
Eric Jackson <ejackson@suse.com>
2017-04-05 00:38:59 -0400

    Fixing check
    Signed-off-by: Eric Jackson <ejackson@suse.com>

--- srv/modules/runners/disengage.py
+++ srv/modules/runners/disengage.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
+"""
+Some operations are inherently dangerous, but still necessary.  Allow
+the modification timestamp of a file to give a window in which to run.
+"""
+
 import os
 import time
+
 
 class SafetyFile(object):
     """
@@ -11,23 +17,24 @@ class SafetyFile(object):
     def __init__(self, cluster):
         self.filename = "/run/salt/master/safety.{}".format(cluster)
 
-def safety(cluster = 'ceph'):
+
+def safety(cluster='ceph'):
     """
     Touch a file.  Need to allow cluster setting from environment.
     """
-    s = SafetyFile(cluster)
-    with open(s.filename, "w") as safe_file:
+    sff = SafetyFile(cluster)
+    with open(sff.filename, "w") as safe_file:
         safe_file.write("")
         return "safety is now disabled for cluster {}".format(cluster)
 
 
-def check(cluster = 'ceph'):
+def check(cluster='ceph'):
     """
     Check that time stamp of file is less than one minute
     """
-    s = SafetyFile(cluster)
-    stamp = os.stat(s.filename).st_mtime
-    return stamp + 60 > time.time()
-
-
-    
+    sff = SafetyFile(cluster)
+    if os.path.exists(sff.filename):
+        stamp = os.stat(sff.filename).st_mtime
+        return stamp + 60 > time.time()
+    else:
+        return False
