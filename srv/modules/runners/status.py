6b9799b6d9c19fabd5c40eba7aa131a7fb01e294
Joshua Schmid <jschmid@suse.de>
2017-05-10 13:06:53 +0200

    detailed status.report with off-minions

--- srv/modules/runners/status.py
+++ srv/modules/runners/status.py
@@ -1,7 +1,6 @@
 import salt.client
 from collections import Counter
 
-
 def _get_data(cluster_name='ceph'):
     local = salt.client.LocalClient()
     status_report = {}
@@ -15,32 +14,7 @@ def _get_data(cluster_name='ceph'):
 
     return os_codename, salt_version, ceph_version
 
-def longreport(cluster_name='ceph'):
-    """
-    Scans for:
-    * OS Version and Codename
-    * Ceph version
-    * Salt version
-    and generates a report
-    """
-    
-    status_report = {}
-
-    os_codename, salt_version, ceph_version = _get_data(cluster_name)
-    # Safely pre-populating the dict as 'search' is equivalent for all queries
-    for node in os_codename.keys():
-       status_report[node] = {}
-    
-    for node,key in os_codename.iteritems():
-       status_report[node]['os_version'] = key
-    for node,key in ceph_version.iteritems():
-       status_report[node]['ceph_version'] = key
-    for node,key in salt_version.iteritems():
-       status_report[node]['salt_version'] = key
-
-    return status_report
-
-def summary(cluster_name='ceph'):
+def report(cluster_name='ceph'):
     """
     Creates a report that tries to find the most common versions from:
       * OS Version and Codename
@@ -51,8 +25,7 @@ def summary(cluster_name='ceph'):
     one of the most_common_versions
     """
     os_codename, salt_version, ceph_version = _get_data(cluster_name)
-
-    unsynced_nodes = {'out of sync': []}
+    unsynced_nodes = {'out of sync': {}}
     common_keys = {'ceph': {}, 'salt': {}, 'os': {}}
 
     def _organize(minion_data):
@@ -62,12 +35,13 @@ def summary(cluster_name='ceph'):
         if counter_obj.most_common():
             most_common_item = counter_obj.most_common()[0][0]
         common_keys.update({key_ident: most_common_item})
-        for node, value in minion_data_dct.iteritems():
-            if value != most_common_item:
-                if node not in unsynced_nodes:
-                    unsynced_nodes['out of sync'].append(node)
+	for node, value in minion_data_dct.iteritems():
+	    if value != most_common_item:
+	        if node not in unsynced_nodes['out of sync'].keys():
+                    unsynced_nodes['out of sync'][node] = {}
+	        unsynced_nodes['out of sync'][node].update({key_ident: value})
 
     for minion_data in [('os', os_codename), ('ceph', ceph_version), ('salt', salt_version)]:
         _organize(minion_data)
         
-    return {'statusreport': [unsynced_nodes, common_keys]}
+    return {'statusreport': [common_keys, unsynced_nodes]}
