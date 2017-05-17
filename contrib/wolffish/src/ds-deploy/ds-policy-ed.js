a13dc0875b93bc346b52d078e9d16aebf18858e7
Karol Mroz <kmroz@suse.de>
2017-05-17 11:33:13 -0700

    wf/ds-deploy: add initial policy editor stub element
    Signed-off-by: Karol Mroz <kmroz@suse.de>

---
+++ contrib/wolffish/src/ds-deploy/ds-policy-ed.js
@@ -0,0 +1,16 @@
+// -----------------------------------------------------------------------------
+//    ds-policy-ed.js
+//
+//    DeepSea policy.cfg editor element.
+// -----------------------------------------------------------------------------
+
+Polymer({
+    is: "ds-policy-ed",
+
+    properties: {
+    },
+
+    ready: function() {
+        console.log(this.is + ": ready.");
+    }
+});
