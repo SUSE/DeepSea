2c88df5ba4f9d9e15c8ffca5122d0644c45e20d4
Karol Mroz <kmroz@suse.de>
2017-05-17 11:34:03 -0700

    wf/ds-deploy: add initial deployment stub element
    Signed-off-by: Karol Mroz <kmroz@suse.de>

---
+++ contrib/wolffish/src/ds-deploy/ds-deploy.js
@@ -0,0 +1,16 @@
+// -----------------------------------------------------------------------------
+// ds-deploy.js
+//
+// The beginnings of a cluster deployment element.
+// -----------------------------------------------------------------------------
+
+Polymer({
+    is: "ds-deploy",
+
+    properties: {
+    },
+
+    ready: function() {
+        console.log(this.is + ": ready.");
+    }
+});
