3a39babf86cda020a8be83e24add58731d39f234
Nathan Cutler <ncutler@suse.com>
2017-06-10 22:28:03 +0200

    qa: run cephfs test routine on client minion
    Before, we were running it on the master node, which was suboptimal because the
    master node might be part of the Ceph cluster.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- qa/common/common.sh
+++ qa/common/common.sh
@@ -21,10 +21,10 @@ function _run_stage {
   echo ""
 
   salt-run --no-color state.orch ceph.stage.${stage_num} | tee /tmp/stage.${stage_num}.log
-  STAGE_FINISHED=$(fgrep 'Total states run' /tmp/stage.${stage_num}.log)
+  STAGE_FINISHED=$(grep -F 'Total states run' /tmp/stage.${stage_num}.log)
 
   if [[ ! -z $STAGE_FINISHED ]]; then
-    FAILED=$(fgrep 'Failed: ' /tmp/stage.${stage_num}.log | sed 's/.*Failed:\s*//g' | head -1)
+    FAILED=$(grep -F 'Failed: ' /tmp/stage.${stage_num}.log | sed 's/.*Failed:\s*//g' | head -1)
     if [[ "$FAILED" -gt "0" ]]; then
       echo "********** Stage $stage_num failed with $FAILED failures **********"
       echo "Check /tmp/stage.${stage_num}.log for details"
@@ -98,21 +98,44 @@ function ceph_health_test {
   ceph -s | tee /dev/stderr | grep -q 'HEALTH_OK\|HEALTH_WARN'
 }
 
+function _client_node {
+  #
+  # FIXME: migrate this to "salt --static --out json ... | jq ..."
+  #
+  salt --no-color -C 'not I@roles:storage' test.ping | grep -o -P '^\S+(?=:)' | head -1
+}
+
+function _mds_node {
+  salt --no-color -C 'I@roles:mds' test.ping | grep -o -P '^\S+(?=:)' | head -1
+}
+
 function cephfs_mount_and_sanity_test {
   #
+  # run cephfs mount test script on the client node
   # mounts cephfs in /mnt, touches a file, asserts that it exists
   #
-  local mons=$(ceph-conf --lookup 'mon_initial_members' | tr -d '[:space:]')
-  local secr=$(grep 'key =' /etc/ceph/ceph.client.admin.keyring | awk '{print $NF}')
-  #sleep 10
-  echo "Running as: $(whoami)"
-  echo "Mounting cephfs"
-  echo "MONs: $mons"
-  echo "admin secret: $secr"
-  test -d /mnt
-  mount -t ceph ${mons}:/ /mnt -o name=admin,secret="$secr"
-  touch /mnt/bubba
-  test -f /mnt/bubba
-  umount /mnt
+  local TESTSCRIPT=/tmp/cephfs_test.sh
+  cat << 'EOF' > $TESTSCRIPT
+set -ex
+trap 'echo "Result: NOT_OK"' ERR
+echo "cephfs mount test script running as $(whoami) on $(hostname --fqdn)"
+TESTMONS=$(ceph-conf --lookup 'mon_initial_members' | tr -d '[:space:]')
+TESTSECR=$(grep 'key =' /etc/ceph/ceph.client.admin.keyring | awk '{print $NF}')
+echo "MONs: $TESTMONS"
+echo "admin secret: $TESTSECR"
+test -d /mnt
+mount -t ceph ${TESTMONS}:/ /mnt -o name=admin,secret="$TESTSECR"
+touch /mnt/bubba
+test -f /mnt/bubba
+umount /mnt
+echo "Result: OK"
+EOF
+  local CLIENTNODE=$(_client_node)
+  # FIXME: assert no MDS running on $CLIENTNODE
+  salt-cp $CLIENTNODE $TESTSCRIPT $TESTSCRIPT
+  local LOGFILE=/tmp/cephfs_test.log
+  salt $CLIENTNODE cmd.run "sh $TESTSCRIPT" | tee $LOGFILE
+  local RESULT=$(grep -o -P '(?<=Result: )(OK|NOT_OK)$' $LOGFILE | head -1)
+  test "x$RESULT" = "xOK"
 }
 
