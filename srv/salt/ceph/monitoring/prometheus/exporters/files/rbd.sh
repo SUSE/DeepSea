b6aae2a3d590cc64cc862c31f0ceef3d46f9f336
Jan Fajerski <jfajerski@suse.com>
2017-07-13 16:23:40 +0200

    monitoring: deploy rbd exporter
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>

--- srv/salt/ceph/monitoring/prometheus/exporters/files/rbd.sh
+++ srv/salt/ceph/monitoring/prometheus/exporters/files/rbd.sh
@@ -9,15 +9,18 @@ awk_cmd='
 {
 print "# HELP ceph_rbd_image_bytes_used Used space of an rbd image"
 print "# TYPE ceph_rbd_image_bytes_used gauge"
-print "ceph_rbd_image_bytes_used{image=\""$1"\",cluster=\""cluster"\"} " $3 * 1024
+print "ceph_rbd_image_bytes_used{image=\""$1"\",cluster=\""cluster"\"} " $3
 print "# HELP ceph_rbd_image_bytes_provisioned Provisioned space of an rbd image"
 print "# TYPE ceph_rbd_image_bytes_provisioned gauge"
-print "ceph_rbd_image_bytes_provisioned{image=\""$1"\",cluster=\""cluster"\"} " $2 * 1024
+print "ceph_rbd_image_bytes_provisioned{image=\""$1"\",cluster=\""cluster"\"} " $2
 }'
 
 for conf in /etc/ceph/*.conf
 do
     filename=$(basename "$conf")
     cluster="${filename%.*}"
-    rbd -c $conf du | sed 1d | awk -v cluster=$cluster "$awk_cmd"
+    rbd --format json -c $conf du |
+    jq ".images[] | [.name, .provisioned_size, .used_size] | @csv" |
+    sed -e 's/^"//' -e 's/"$//' -e 's/\\"//g' | # stripe some quotes
+    awk -F , -v cluster=$cluster "$awk_cmd"
 done
