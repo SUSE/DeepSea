b4432ffaedcf854e2682ea98d211379ba706532e
Alex Lau (AvengerMoJo) <alau@suse.com>
2017-08-18 04:16:49 +0800

    Clean up nettest multi.py format
    Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>

--- srv/salt/_modules/multi.py
+++ srv/salt/_modules/multi.py
@@ -3,8 +3,9 @@
 import logging
 import multiprocessing.dummy
 import multiprocessing
-import re, socket
-from subprocess import check_call, Popen, PIPE, CalledProcessError
+import re
+import socket
+from subprocess import Popen, PIPE
 
 log = logging.getLogger(__name__)
 
@@ -31,6 +32,7 @@ CLI Example:
     sudo salt 'node' multi.ping <hostname>|<ip> <hostname>|<ip>....
 '''
 
+
 def _all(func, hosts):
     '''
     Internal function that allow function to perform in all hosts
@@ -39,12 +41,13 @@ def _all(func, hosts):
     # threads should likely scale with cores or interfaces
     cpus = multiprocessing.cpu_count()
     threads = 4 * cpus
-    log.debug('multi._all cpus count={},  thread count={}'.format(cpus, threads))
+    log.debug('multi._all cpus count={}, thread count={}'.format(cpus, threads))
     pool = multiprocessing.dummy.Pool(threads)
     for instance in pool.map(func, hosts):
         all_instances.append(instance)
 
     return all_instances
+
 
 def _summarize_iperf(result):
     '''
@@ -55,8 +58,10 @@ def _summarize_iperf(result):
     msg['server'] = host
     if rc == 0:
         msg['succeeded'] = True
-    	msg['speed'] = out
-    	msg['filter'] = re.match(r'.*0.00-10.00.*sec\s(.*Bytes)\s+(.*Mbits/sec)', out, re.DOTALL ).group(2)
+        msg['speed'] = out
+        msg['filter'] = re.match(
+            r'.*0.00-10.00.*sec\s(.*Bytes)\s+(.*Mbits/sec)',
+            out, re.DOTALL).group(2)
     if rc == 1:
         msg['succeeded'] = False
         msg['failed'] = True
@@ -64,6 +69,7 @@ def _summarize_iperf(result):
         msg['succeeded'] = False
         msg['errored'] = True
     return msg
+
 
 def _summarize_ping(results):
     '''
@@ -78,9 +84,11 @@ def _summarize_ping(results):
         host, rc, out, err = result
         if rc == 0:
             success.append(host)
-            rtt = re.match(r'.*rtt min/avg/max/mdev = \d+\.?\d+/(\d+\.?\d+)/', out, re.DOTALL)
+            rtt = re.match(
+                r'.*rtt min/avg/max/mdev = \d+\.?\d+/(\d+\.?\d+)/',
+                out, re.DOTALL)
             if rtt:
-               avg.append({'avg':float(rtt.group(1)), 'host':host})
+                avg.append({'avg': float(rtt.group(1)), 'host': host})
         if rc == 1:
             failed.append(host)
         if rc == 2:
@@ -92,11 +100,12 @@ def _summarize_ping(results):
         avg_sum = sum(i.get('avg') for i in avg) / len(avg)
         if len(avg) > 2:
             for i in avg:
-                if (avg_sum * len(avg) / 2) < i.get('avg') :
-                    log.debug('_summarize_ping: slow host running = {} avg = {}, s'.format( i.get('avg'), avg_sum ))
+                if (avg_sum * len(avg) / 2) < i.get('avg'):
+                    log.debug('_summarize_ping: slow host = {} avg = {}, s'.
+                              format(i.get('avg'), avg_sum))
                     slow.append(i.get('host'))
     else:
-        avg_sum = 0 
+        avg_sum = 0
 
     msg = {}
     msg['succeeded'] = len(success)
@@ -109,64 +118,69 @@ def _summarize_ping(results):
     msg['avg'] = avg_sum
     return msg
 
+
 def iperf(server, cpu, port):
     '''
-    iperf test to a specific server 
+    iperf test to a specific server
 
     CLI Example:
     .. code-block:: bash
         sudo salt 'node' multi.iperf <hostname>|<ip> <cpu_core> <port>
     '''
     log.debug('iperf server ={}'.format(server))
-    return _summarize_iperf( iperf_client_cmd( server, cpu, port ) )
-    #return True
+    return _summarize_iperf(iperf_client_cmd(server, cpu, port))
 
-def iperf_client_cmd( server, cpu=0, port=5200 ):
+
+def iperf_client_cmd(server, cpu=0, port=5200):
     '''
     Use iperf to test minion to server
-        
+
     CLI Example:
     .. code-block:: bash
-    salt 'node' multi.iperf_client_cmd <server_name/ip> cpu=<which_cpu_core default 0> port=<default 5200>
+    salt 'node' multi.iperf_client_cmd <server_name/ip>
+            cpu=<which_cpu_core default 0> port=<default 5200>
     '''
     if iperf_path is None or not server:
         if not server:
-            return [ localhost_name, 2, "0", "Server name is empty" ]
+            return [localhost_name, 2, "0", "Server name is empty"]
         else:
-            return [ localhost_name, 2, "0", "iperf3 not found in path, please install" ]
-    iperf_cmd = [ "/usr/bin/iperf3", "-fm", "-A"+str(cpu), "-t10", "-c"+server, "-p"+str(port)]
+            return [localhost_name, 2, "0",
+                    "iperf3 not found in path, please install"]
+    iperf_cmd = ["/usr/bin/iperf3", "-fm", "-A"+str(cpu),
+                 "-t10", "-c"+server, "-p"+str(port)]
     log.debug('iperf_client_cmd: cmd {}'.format(iperf_cmd))
     proc = Popen(iperf_cmd, stdout=PIPE, stderr=PIPE)
     proc.wait()
     return server, proc.returncode, proc.stdout.read(), proc.stderr.read()
 
-def iperf_server_cmd( cpu=0, port=5200 ):
+
+def iperf_server_cmd(cpu=0, port=5200):
     '''
     Use iperf to test minion to server
-        
+
     CLI Example:
     .. code-block:: bash
-    salt 'node' multi.iperf_server_cmd <server_name/ip> cpu=<which_cpu_core default 0> port=<default 5200>
+    salt 'node' multi.iperf_server_cmd <server_name/ip>
+        cpu=<which_cpu_core default 0> port=<default 5200>
     '''
     if iperf_path is None:
         return localhost_name + ": iperf3 not found in path, please install"
-    iperf_cmd = [ "/usr/bin/iperf3", "-s", "-D", "-A"+str(cpu),  "-p"+str(port)]
+    iperf_cmd = ["/usr/bin/iperf3", "-s", "-D", "-A"+str(cpu),  "-p"+str(port)]
     log.debug('iperf_server_cmd: cmd {}'.format(iperf_cmd))
-    #with open( "/var/log/iperf_cpu" + str(cpu) + "_port" + str(port) + ".log", "wb") as out, \
-    #open( "/var/log/iperf_cpu" + str(cpu) + "_port" + str(port) + ".err", "wb" ) as err:
-    #proc = Popen(iperf_cmd, stdout=out, stderr=err)
-    proc = Popen(iperf_cmd);
-    # it doesn't report fail so no need to check 
-    return localhost_name + ": iperf3 started at cpu " +str(cpu) + " port " + str(port) + "\n"
+    Popen(iperf_cmd)
+    # it doesn't report fail so no need to check
+    return localhost_name + ": iperf3 started at cpu " + str(cpu) + " port " + str(port) + "\n"
+
 
 def kill_iperf_cmd():
     '''
-    Clean up all the iperf3 server and clean it. 
+    Clean up all the iperf3 server and clean it.
     '''
-    kill_cmd = [ "/usr/bin/killall", "-9", "iperf3"]
+    kill_cmd = ["/usr/bin/killall", "-9", "iperf3"]
     log.debug('kill_iperf_cmd: cmd {}'.format(kill_cmd))
-    proc = Popen(kill_cmd)
+    Popen(kill_cmd)
     return True
+
 
 def ping(*hosts):
     '''
@@ -176,10 +190,11 @@ def ping(*hosts):
     .. code-block:: bash
         sudo salt 'node' multi.ping <hostname>|<ip> <hostname>|<ip>....
     '''
-    # I should be filter all the localhost here? 
+    # I should be filter all the localhost here?
     log.debug('ping hostlist={}'.format(list(hosts)))
     results = _all(ping_cmd, list(hosts))
     return _summarize_ping(results)
+
 
 def ping_cmd(host):
     '''
@@ -189,11 +204,12 @@ def ping_cmd(host):
     .. code-block:: bash
         sudo salt 'node' multi.ping_cmd <hostname>|<ip>
     '''
-    cmd = [ "/usr/bin/ping", "-c1", "-q", "-W1", host ]
+    cmd = ["/usr/bin/ping", "-c1", "-q", "-W1", host]
     log.debug('ping_cmd hostname={}'.format(host))
     proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
     proc.wait()
     return host, proc.returncode, proc.stdout.read(), proc.stderr.read()
+
 
 def jumbo_ping(*hosts):
     '''
@@ -203,10 +219,11 @@ def jumbo_ping(*hosts):
     .. code-block:: bash
         sudo salt 'node' multi.ping <hostname>|<ip> <hostname>|<ip>....
     '''
-    # I should be filter all the localhost here? 
+    # I should be filter all the localhost here?
     log.debug('jumbo_ping hostlist={}'.format(list(hosts)))
     results = _all(jumbo_ping_cmd, list(hosts))
     return _summarize_ping(results)
+
 
 def jumbo_ping_cmd(host):
     '''
@@ -216,11 +233,12 @@ def jumbo_ping_cmd(host):
     .. code-block:: bash
         sudo salt 'node' multi.ping_cmd <hostname>|<ip>
     '''
-    cmd = [ "/usr/bin/ping", "-Mdo", "-s8972", "-c1", "-q", "-W1", host ]
+    cmd = ["/usr/bin/ping", "-Mdo", "-s8972", "-c1", "-q", "-W1", host]
     log.debug('ping_cmd hostname={}'.format(host))
     proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
     proc.wait()
     return host, proc.returncode, proc.stdout.read(), proc.stderr.read()
+
 
 def prepare_iperf_server():
     '''
@@ -228,11 +246,11 @@ def prepare_iperf_server():
 
     CLI Example:
     .. code-block:: bash
-    salt 'node' multi.prepare_iperf_server 
+    salt 'node' multi.prepare_iperf_server
 
     '''
     cpus = multiprocessing.cpu_count()
     iperf_log = ""
     for cpu in range(cpus):
-        iperf_log += iperf_server_cmd( cpu, 5200+cpu ) 
+        iperf_log += iperf_server_cmd(cpu, 5200+cpu)
     return iperf_log
