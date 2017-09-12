6d8ae4833889d890cccb02509242858d80af357c
Volker Theile <vtheile@suse.com>
2017-09-12 14:55:38 +0200

    Provide additional RGW stats: number of users/buckets (OP-2585). https://tracker.openattic.org/browse/OP-2585
    - Add a RGW Grafana dashboard to display user and bucket count
    - Only install the RGW Grafana dashboards if there is a RGW node configured
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- srv/salt/ceph/monitoring/prometheus/exporters/files/ceph_rgw.py
+++ srv/salt/ceph/monitoring/prometheus/exporters/files/ceph_rgw.py
@@ -11,17 +11,31 @@ import syslog
 import time
 
 class CephRgwCollector(object):
-    def _request_data(self):
+    def _exec_rgw_admin(self, args):
         try:
-            out = subprocess.check_output(['radosgw-admin', 'usage', 'show',
-                '--show-log-sum=false'])
+            out = subprocess.check_output(['radosgw-admin'] + args)
             return json.loads(out.decode())
         except Exception as e:
             syslog.syslog(syslog.LOG_ERR, str(e))
             return {}
 
+    def _collect_user_list(self):
+        return self._exec_rgw_admin(['metadata', 'list', 'user'])
+
+    def _collect_bucket_list(self):
+        return self._exec_rgw_admin(['bucket', 'list'])
+
+    def _collect_usage_data(self):
+        return self._exec_rgw_admin(['usage', 'show', '--show-log-sum=false'])
+
     def _init_metrics(self):
         self._metrics = {
+            'user_count': prometheus_client.core.CounterMetricFamily(
+                'ceph_rgw_user_count',
+                'Number of users'),
+            'bucket_count': prometheus_client.core.CounterMetricFamily(
+                'ceph_rgw_bucket_count',
+                'Number of buckets'),
             'ops': prometheus_client.core.CounterMetricFamily(
                 'ceph_rgw_user_usage_ops_total',
                 'Number of operations',
@@ -40,7 +54,13 @@ class CephRgwCollector(object):
                 labels=["bucket", "owner", "category"])
         }
 
-    def _add_metrics(self, bucket_name, bucket_owner, category):
+    def _add_user_metrics(self, count):
+        self._metrics['user_count'].add_metric([], count)
+
+    def _add_bucket_metrics(self, count):
+        self._metrics['bucket_count'].add_metric([], count)
+
+    def _add_usage_metrics(self, bucket_name, bucket_owner, category):
         self._metrics['ops'].add_metric([
             bucket_name, bucket_owner,
             category['category']], category['ops'])
@@ -55,13 +75,20 @@ class CephRgwCollector(object):
             category['category']], category['bytes_received'])
 
     def collect(self):
-        data = self._request_data()
         self._init_metrics()
+        # Process number of users.
+        data = self._collect_user_list()
+        self._add_user_metrics(len(data))
+        # Process number of buckets.
+        data = self._collect_bucket_list()
+        self._add_bucket_metrics(len(data))
+        # Process the usage statistics.
+        data = self._collect_usage_data()
         if 'entries' in data:
             for entry in data['entries']:
                 for bucket in entry['buckets']:
                     for category in bucket['categories']:
-                        self._add_metrics(
+                        self._add_usage_metrics(
                             bucket['bucket'],
                             bucket['owner'],
                             category)
