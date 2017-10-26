SUSE/DeepSea

commit 1e99b8ec8eb7ac98cf1ddd787469858ac5f42e54

qa: add basic restart mechanism for ceph.restart tests
Implementing basic validation for restarted services
for now only: MON, OSD, RGW

Signed-off-by: Joshua Schmid <jschmid@suse.de>

qa/common/common.sh
@@ -113,6 +113,24 @@ function run_stage_5 {
 # see https://github.com/SUSE/DeepSea/tree/master/srv/salt/ceph/configuration/files/ceph.conf.d
 #
 
+function change_rgw_conf {
+    cat <<EOF >> /srv/salt/ceph/configuration/files/ceph.conf.d/rgw.conf
+foo = bar
+EOF
+}
+
+function change_osd_conf {
+    cat <<EOF >> /srv/salt/ceph/configuration/files/ceph.conf.d/osd.conf
+foo = bar
+EOF
+}
+
+function change_mon_conf {
+    cat <<EOF >> /srv/salt/ceph/configuration/files/ceph.conf.d/mon.conf
+foo = bar
+EOF
+}
+
 function ceph_conf_small_cluster {
   local STORAGENODES=$(json_storage_nodes)
   test ! -z "$STORAGENODES"
@@ -149,7 +167,45 @@ EOF
 #
 
 function proposal_populate_dmcrypt {
-    salt-run proposal.populate encryption='dmcrypt' name='dmcrypt'
+  salt-run proposal.populate encryption='dmcrypt' name='dmcrypt'
+}
+
+#
+# functions for restarting all configured services
+#
+
+function restart_services {
+  salt-run state.orch ceph.restart
+}
+
+function mon_restarted {
+  local expected_return=$1
+  local mon_hosts=$(salt --static --out json -C "I@roles:mon" test.ping | jq -r 'keys[]')
+  for minion in ${mon_hosts}; do
+    salt "${minion}*" cmd.shell "journalctl -u ceph-mon@*" | grep -i terminated
+    test $? = ${expected_return}
+  done
+}
+
+function osd_restarted {
+    local expected_return=$1
+    osd_hosts=$(salt --static --out json -C "I@roles:storage" test.ping | jq -r 'keys[]')
+    for host in ${osd_hosts}; do
+        osds=$(salt --static --out json ${host} osd.list | jq .[][])
+        for osd in ${osds}; do
+            salt ${host} cmd.shell "journalctl -u ceph-osd@${osd}" | grep -i terminated
+            test $? = ${expected_return}
+        done
+    done
+}
+
+function rgw_restarted {
+  local expected_return=$1
+  rgw_hosts=$(salt --static --out json -C "I@roles:rgw" test.ping | jq -r 'keys[]')
+  for host in ${rgw_hosts}; do
+    salt ${host} cmd.shell "journalctl -u ceph-radosgw@*" | grep -i terminated
+    test $? = ${expected_return}
+  done
 }
 
 #

qa/suites/basic/health-ok.sh
@@ -72,5 +72,16 @@ test_systemd_ceph_osd_target_wants
 rados_write_test
 ceph_version_test
 run_stage_0 "$CLI"
+restart_services
+mon_restarted "1" # 1 means not restarted
+osd_restarted "1"
+# apply config change
+change_osd_conf
+change_mon_conf
+# construct and spread config
+run_stage_3 "$CLI"
+restart_services
+mon_restarted "0" # 0 means restarted
+osd_restarted "0"
 
 echo "OK"

qa/suites/basic/health-rgw.sh
@@ -94,5 +94,13 @@ rgw_user_and_bucket_list
 rgw_validate_demo_users
 ceph_health_test
 run_stage_0 "$CLI"
+restart_services
+rgw_restarted "1" # 1 means not restarted
+# apply config change
+change_rgw_conf
+# construct and spread config
+run_stage_3 "$CLI"
+restart_services
+rgw_restarted "0" # 0 means restarted
 
 echo "OK"

commit 5db6c38292abfe9b460d958d4ff1ddd2be419383

qa: fix regression introduced by 1e99b8ec8eb7ac98cf1ddd787469858ac5f42e54
Since the qa scripts run with "set -e" in effect, the script was failing
whenever the *_restarted functions were called with expected_return != 0.

Signed-off-by: Nathan Cutler <ncutler@suse.com>

qa/common/common.sh
@@ -174,7 +174,7 @@ function proposal_populate_dmcrypt {
 }
 
 #
-# functions for restarting all configured services
+# functions for testing ceph.restart orchestration
 #
 
 function restart_services {
@@ -184,15 +184,18 @@ function restart_services {
 function mon_restarted {
   local expected_return=$1
   local mon_hosts=$(salt --static --out json -C "I@roles:mon" test.ping | jq -r 'keys[]')
+  set +e
   for minion in ${mon_hosts}; do
     salt "${minion}*" cmd.shell "journalctl -u ceph-mon@*" | grep -i terminated
     test $? = ${expected_return}
   done
+  set -e
 }
 
 function osd_restarted {
     local expected_return=$1
     osd_hosts=$(salt --static --out json -C "I@roles:storage" test.ping | jq -r 'keys[]')
+    set +e
     for host in ${osd_hosts}; do
         osds=$(salt --static --out json ${host} osd.list | jq .[][])
         for osd in ${osds}; do
@@ -200,15 +203,18 @@ function osd_restarted {
             test $? = ${expected_return}
         done
     done
+    set -e
 }
 
 function rgw_restarted {
   local expected_return=$1
   rgw_hosts=$(salt --static --out json -C "I@roles:rgw" test.ping | jq -r 'keys[]')
+  set +e
   for host in ${rgw_hosts}; do
     salt ${host} cmd.shell "journalctl -u ceph-radosgw@*" | grep -i terminated
     test $? = ${expected_return}
   done
+  set -e
 }
 
 #