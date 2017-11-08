0f638c36495fe08cdc480364a937d3f056ede513
Joshua Schmid <jschmid@suse.de>
2017-11-08 13:17:49 +0100

    Safety net for a failed engulfment
    Signed-off-by: Joshua Schmid <jschmid@suse.de>

--- srv/modules/runners/advise.py
+++ srv/modules/runners/advise.py
@@ -65,6 +65,21 @@ def salt_upgrade():
     return message
 
 
+def no_cluster_detected():
+    """
+    Advise the installer that if the upgrade fails, rerun the orchestration.
+    """
+    message = '''
+        *************** PLEASE READ ***********************
+        You triggered an update but we couldn't find any
+        trace of a ceph cluster. Please make sure to have
+        setup DeepSea correctly and start the upgrade again.
+        ***************************************************'''
+
+    print message
+    return message
+
+
 def networks():
     """
     Advise the installer the current network settings.
