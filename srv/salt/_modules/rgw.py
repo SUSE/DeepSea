SUSE/DeepSea

commit 0b5116e2042116c23287783795a11408839652f4

Lint fixes
Signed-off-by: Eric Jackson <ejackson@suse.com>

srv/salt/_modules/rgw.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=fixme
+"""
+RadosGW related functions for users, configurations, keys and buckets
+"""
 
-import salt.config
+from __future__ import absolute_import
 import logging
+# pylint: disable=incompatible-py3-code
 from subprocess import Popen, PIPE
 import os
 import json
-import boto
-import boto.s3.connection
-import boto.exception
-import glob
 import re
+# pylint: disable=import-error,3rd-party-module-not-gated
+import boto
+# pylint: disable=import-error,3rd-party-module-not-gated
+import boto.s3.connection
+# pylint: disable=import-error,3rd-party-module-not-gated
+import boto.exception
+import salt.config
 
 log = logging.getLogger(__name__)
+
 
 def configurations():
     """
@@ -28,7 +37,7 @@ def configurations():
                         set(__pillar__['roles']))
 
         if 'rgw' in __pillar__['roles']:
-            return [ 'rgw' ]
+            return ['rgw']
     return []
 
 
@@ -41,11 +50,10 @@ def configuration(role):
         return 'rgw'
     if 'roles' in __pillar__:
         if 'rgw_configurations' in __pillar__:
-            for rgw_config in  __pillar__['rgw_configurations']:
+            for rgw_config in __pillar__['rgw_configurations']:
                 if rgw_config in role:
                     return rgw_config
     return
-
 
 
 def users(realm='default', contains=None):
@@ -59,9 +67,10 @@ def users(realm='default', contains=None):
     log.debug("rc: {}".format(proc.returncode))
     if proc.returncode != '0':
         if contains:
-            return [ item for item in json.loads(proc.stdout.read()) if contains in item]
+            return [item for item in json.loads(proc.stdout.read()) if contains in item]
         return json.loads(proc.stdout.read())
     return []
+
 
 def add_users(pathname="/srv/salt/ceph/rgw/cache", jinja="/srv/salt/ceph/rgw/files/users.j2"):
     """
@@ -85,47 +94,52 @@ def add_users(pathname="/srv/salt/ceph/rgw/cache", jinja="/srv/salt/ceph/rgw/fil
 
             # Create the RGW user if it does not exist.
             if not user['uid'] in existing_users:
-                base_cmd = "radosgw-admin user create --uid={uid} " \
-                    "--display-name={name} --rgw-realm={realm}".format(
-                    uid=user['uid'], name=user['name'], realm=realm)
-
+                base_cmd = ("radosgw-admin user create --uid={uid} "
+                            "--display-name={name} "
+                            "--rgw-realm={realm}".format(uid=user['uid'],
+                                                         name=user['name'],
+                                                         realm=realm))
                 args = ''
                 if 'email' in user:
-                    args += " --email=%s" % user['email']
-
-                if 'system' in user and user['system'] is True:
-                    args += " --system"
-
+                    args += " --email={}".format(user['email'])
                 if 'access_key' in user:
-                    args += " --access-key=%s" % user['access_key']
+                    args += " --access-key={}".format(user['access_key'])
 
                 if 'secret' in user:
-                    args += " --secret=%s" % user['secret']
-
+                    args += " --secret={}".format(user['secret'])
                 command = base_cmd + args
-
                 proc = Popen(command.split(), stdout=PIPE, stderr=PIPE)
-                with open(filename, "w") as json:
+                filename = "{}/user.{}.json".format(pathname, user['uid'])
+                with open(filename, "w") as _json:
                     for line in proc.stdout:
-                        json.write(line)
+                        _json.write(line)
                 for line in proc.stderr:
                     log.info("stderr: {}".format(line))
+                    proc = Popen(command.split(), stdout=PIPE, stderr=PIPE)
+                    with open(filename, "w") as _json:
+                        for line in proc.stdout:
+                            _json.write(line)
+                    for line in proc.stderr:
+                        log.info("stderr: {}".format(line))
 
                 proc.wait()
             else:
                 # Create the JSON file if it does not exist. This happens
                 # when the RGW user was manually created beforehand.
+                # pylint: disable=useless-else-on-loop
                 if not os.path.exists(filename):
+                    # pylint: disable=redefined-variable-type
                     args = ['radosgw-admin', 'user', 'info']
                     args.extend(['--uid', user['uid']])
                     args.extend(['--rgw-realm', realm])
                     proc = Popen(args, stdout=PIPE, stderr=PIPE)
-                    with open(filename, "w") as json:
+                    with open(filename, "w") as _json:
                         for line in proc.stdout:
-                            json.write(line)
+                            _json.write(line)
                     for line in proc.stderr:
                         log.info("stderr: {}".format(line))
                     proc.wait()
+
 
 def _key(user, field, pathname):
     """
@@ -142,16 +156,27 @@ def _key(user, field, pathname):
 
     return data['keys'][0][field]
 
+
 def access_key(user, pathname="/srv/salt/ceph/rgw/cache"):
+    """
+    Returns the access key for a given user
+    """
     if not user:
         raise ValueError("ERROR: no user specified")
     return _key(user, 'access_key', pathname)
 
+
 def secret_key(user, pathname="/srv/salt/ceph/rgw/cache"):
+    """
+    Returns the secret key for a given user
+    """
     return _key(user, 'secret_key', pathname)
 
 
 def endpoints(cluster='ceph'):
+    """
+    Returns an array of data structures for each gateway
+    """
     result = []
 
     search = "I@cluster:{}".format(cluster)
@@ -208,6 +233,7 @@ def endpoints(cluster='ceph'):
                 if line:
                     match = re.search(r'rgw.*frontends.*=.*port=(\d+)(s?)', line)
                     if match:
+                        # pylint: disable=redefined-variable-type
                         port = int(match.group(1))
                         ssl = match.group(2)
 
@@ -217,7 +243,8 @@ def endpoints(cluster='ceph'):
 
         local = salt.client.LocalClient()
 
-        fqdns = local.cmd('I@roles:'+ rgw_conf_files[pathname], 'grains.item', ['fqdn'], expr_form="compound")
+        fqdns = local.cmd('I@roles:'+ rgw_conf_files[pathname], 'grains.item',
+                          ['fqdn'], expr_form="compound")
         for _, grains in fqdns.items():
             log.warning("fqdns: {}".format(fqdns))
             result.append({
@@ -230,6 +257,9 @@ def endpoints(cluster='ceph'):
 
 
 def s3connect(user):
+    """
+    Return an S3 connection
+    """
     endpoint = endpoints()[0]
 
     s3conn = boto.connect_s3(
@@ -242,7 +272,11 @@ def s3connect(user):
     )
     return s3conn
 
+
 def create_bucket(**kwargs):
+    """
+    Create a bucket for a user
+    """
     s3conn = s3connect(kwargs['user'])
     try:
         s3conn.create_bucket(kwargs['bucket_name'])
@@ -250,7 +284,11 @@ def create_bucket(**kwargs):
         return False
     return True
 
+
 def lookup_bucket(user, bucket):
+    """
+    Query a bucket for a user
+    """
     s3conn = s3connect(user)
     if s3conn.lookup(bucket, validate=True) is None:
         return False

Makefile
@@ -580,3 +580,6 @@ tarball:
 
 test:
 	tox -e py27
+
+lint:
+	tox -e lint

srv/__init__.py
@@ -1,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# vim: ts=8 et sw=4 sts=4

srv/modules/__init__.py
@@ -1,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# vim: ts=8 et sw=4 sts=4

srv/modules/runners/__init__.py
@@ -1,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# vim: ts=8 et sw=4 sts=4

srv/modules/runners/advise.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
-
-import salt.client
-import time
-import logging
-import os
-
-log = logging.getLogger(__name__)
-
-
+# pylint: disable=modernize-parse-error
 """
 Some steps surprise new users.  This runner should print nice messages to
 explain those steps to the unwary.  There is a module with the same name
@@ -16,7 +8,13 @@ and purpose but different functions.
 Note: a runner's output displays immediately unlike a module
 """
 
-def help():
+import logging
+import salt.client
+
+log = logging.getLogger(__name__)
+
+
+def help_():
     """
     Usage
     """
@@ -28,10 +26,10 @@ def help():
              '\n\n'
              'salt-run advise.networks:\n\n'
              '    Passive message about public and cluster networks\n'
-             '\n\n'
-    )
+             '\n\n')
     print usage
     return ""
+
 
 def salt_run():
     """
@@ -66,13 +64,14 @@ def salt_upgrade():
     print message
     return message
 
+
 def networks():
     """
     Advise the installer the current network settings.
     """
     local = salt.client.LocalClient()
-    public = set(local.cmd('*' , 'pillar.get', [ 'public_network' ]).values())
-    cluster = set(local.cmd('*' , 'pillar.get', [ 'cluster_network' ]).values())
+    public = set(local.cmd('*', 'pillar.get', ['public_network']).values())
+    cluster = set(local.cmd('*', 'pillar.get', ['cluster_network']).values())
 
     bold = '\033[1m'
     endc = '\033[0m'
@@ -81,3 +80,6 @@ def networks():
     print "{:25}: {}{}{}".format('cluster network', bold, ", ".join(filter(None, cluster)), endc)
     return ""
 
+__func_alias__ = {
+                 'help_': 'help',
+                 }

srv/modules/runners/benchmark.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# pylint: skip-file
 
 import salt.client
 import salt.config

srv/modules/runners/cephprocesses.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-
-import salt.client
-import pprint
-import os
-import sys
-import logging
-import salt.utils
-import salt.utils.master
-
-log = logging.getLogger(__name__)
-
+# pylint: disable=modernize-parse-error
 """
 The original purpose of this runner is to verify that proceeding with an
 upgrade is safe.  All expected processes are running.
@@ -17,7 +7,18 @@ upgrade is safe.  All expected processes are running.
 A secondary purpose is a utility to check the current state of all processes.
 """
 
-def help():
+import pprint
+import os
+import sys
+import logging
+import salt.client
+import salt.utils
+import salt.utils.master
+
+log = logging.getLogger(__name__)
+
+
+def help_():
     """
     Usage
     """
@@ -29,12 +30,12 @@ def help():
              '\n\n'
              'salt-run cephprocesses.wait:\n\n'
              '    Wait for all processes to be up according to assigned roles\n'
-             '\n\n'
-    )
+             '\n\n')
     print usage
     return ""
 
 
+# pylint: disable=dangerous-default-value
 def check(cluster='ceph', roles=[], tolerate_down=0, verbose=True):
     """
     Query the status of running processes for each role.  Also, verify that
@@ -52,7 +53,7 @@ def check(cluster='ceph', roles=[], tolerate_down=0, verbose=True):
 
     ret = True
 
-    for role in status.keys():
+    for role in status:
         for minion in status[role]:
             if status[role][minion] is False:
                 if tolerate_down == 0:
@@ -62,6 +63,7 @@ def check(cluster='ceph', roles=[], tolerate_down=0, verbose=True):
 
     return ret
 
+
 def mon(cluster='ceph'):
     """
     Query all monitors.  If any are running, assume cluster is running and
@@ -69,11 +71,11 @@ def mon(cluster='ceph'):
     to determines whether minion steps should happen serially or in parallel.
     """
     status = _status("I@cluster:{}".format(cluster), ['mon'], False)
-    running = False
     for minion in status['mon']:
         if status['mon'][minion]:
             return True
     return False
+
 
 def _status(search, roles, verbose):
     """
@@ -94,7 +96,6 @@ def _status(search, roles, verbose):
                                  verbose=verbose,
                                  expr_form="compound")
 
-
     sys.stdout = _stdout
     log.debug(pprint.pformat(status))
     return status
@@ -114,7 +115,7 @@ def _cached_roles(search):
 
     cached = pillar_util.get_minion_pillar()
     roles = {}
-    for minion in cached.keys():
+    for minion in cached:
         if 'roles' in cached[minion]:
             for role in cached[minion]['roles']:
                 roles.setdefault(role, []).append(minion)
@@ -146,9 +147,9 @@ def wait(cluster='ceph', **kwargs):
                        expr_form="compound")
 
     sys.stdout = _stdout
-    log.debug("status: ".format(pprint.pformat(status)))
+    log.debug("status: {}".format(pprint.pformat(status)))
     if False in status.values():
-        for minion in status.keys():
+        for minion in status:
             if status[minion] is False:
                 log.error("minion {} failed".format(minion))
         return False
@@ -167,3 +168,7 @@ def _timeout(cluster='ceph'):
         return 900
     else:
         return 120
+
+__func_alias__ = {
+                 'help_': 'help',
+                 }

commit 9b550f876f728e14ced3f75f1bc0f510ace492af

radosgw: fix creation of radosgw system user
Fixes a regression introduced in commit 0b5116e

Fixes: #783

Signed-off-by: Ricardo Dias <rdias@suse.com>

srv/salt/_modules/rgw.py
@@ -102,6 +102,8 @@ def add_users(pathname="/srv/salt/ceph/rgw/cache", jinja="/srv/salt/ceph/rgw/fil
                 args = ''
                 if 'email' in user:
                     args += " --email={}".format(user['email'])
+                if 'system' in user and user['system']:
+                    args += " --system"
                 if 'access_key' in user:
                     args += " --access-key={}".format(user['access_key'])
 