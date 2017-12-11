908f22871deef6fe02a059d2eeff0b57650b2bbf
Jan Fajerski <jfajerski@suse.com>
2017-12-11 10:50:21 +0100

    runner/deepsea: add option for json output
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>
    (cherry picked from commit ab1d5d58b08907211bb8fd58e2964c5aed1e0db2)

--- srv/modules/runners/deepsea.py
+++ srv/modules/runners/deepsea.py
@@ -3,12 +3,27 @@
 This runner implements helper functions for DeepSea in general
 """
 
+from __future__ import absolute_import
+import re
+
 DEEPSEA_VERSION = 'DEVVERSION'
 
 
+# pylint: disable=redefined-builtin
 def version(**kwargs):
-    # pylint: disable=unused-argument
     """
     Returns the DeepSea version info currently installed
     """
+    format_ = kwargs['format'] if 'format' in kwargs else 'plain'
+
+    if format_ == 'json':
+        ver = re.findall(r'^\d\.\d\.?\d?', DEEPSEA_VERSION)
+        offset = re.findall(r'\+\d+', DEEPSEA_VERSION)
+        hash_ = re.findall(r'[\w]{7,8}$', DEEPSEA_VERSION)
+
+        return {'full_version': DEEPSEA_VERSION,
+                'version': ver[0] if ver else '0.0.0',
+                'git_offset': offset[0].lstrip('+') if offset else '0',
+                'git_hash': hash_[0][-7:] if hash_ else ''}
+
     return DEEPSEA_VERSION
