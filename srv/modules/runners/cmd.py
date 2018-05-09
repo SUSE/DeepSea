f7b569853fc8c4cbb0ca4ad1c6c3577176936035
Jan Fajerski <jfajerski@suse.com>
2018-05-09 11:25:27 +0200

    cmd runner: adapt to removed master_minion pillar
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>

--- srv/modules/runners/cmd.py
+++ srv/modules/runners/cmd.py
@@ -6,8 +6,6 @@ def run(**kwargs):
     args = {'cmd': 'echo no command supplied'}
     args.update(kwargs)
     local_client = salt.client.LocalClient()
-    master_minion = local_client.cmd(
-        'I@roles:master', 'pillar.get',
-        ['master_minion'], expr_form='compound').items()[0][1]
-    output = local_client.cmd(master_minion, 'cmd.shell', [args['cmd']], expr_form='compound')
+    target = 'I@roles:admin'
+    output = local_client.cmd(target, 'cmd.shell', [args['cmd']], expr_form='compound')
     return next(iter(output.values()))
