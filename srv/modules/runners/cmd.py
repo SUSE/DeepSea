a4299aa097b45c2107c54e28047d4c36d7f143e9
Jan Fajerski <jfajerski@suse.com>
2018-05-25 11:20:27 +0200

    runners/cmd: fix lint issues
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>

--- srv/modules/runners/cmd.py
+++ srv/modules/runners/cmd.py
@@ -1,8 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+This runner enables any minion to run a command on an admin node. Most notably
+this enables minions to execute ceph commands that needs an admin keyring.
+"""
+from __future__ import absolute_import
 import salt.client
 
-from subprocess import check_output
 
 def run(**kwargs):
+    """
+    Run an arbitrary command on a admin node (any node that has a admin
+    keyring)
+    """
     args = {'cmd': 'echo no command supplied'}
     args.update(kwargs)
     local_client = salt.client.LocalClient()
