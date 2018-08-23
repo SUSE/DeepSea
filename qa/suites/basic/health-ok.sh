6306ba7ca68665448517802e1f7e6bb9df5fa03d
Nathan Cutler <ncutler@suse.com>
2018-08-23 19:55:17 +0200

    qa/health-ok: fix up the comment block at the top
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- qa/suites/basic/health-ok.sh
+++ qa/suites/basic/health-ok.sh
@@ -3,14 +3,16 @@
 # DeepSea integration test "suites/basic/health-ok.sh"
 #
 # This script runs DeepSea stages 0-3 (or 0-4, depending on options) to deploy
-# a Ceph cluster, optionally with RGW, MDS, and/or data-at-rest encryption of
-# OSDs, on all the nodes that have at least one external disk drive. After the
-# last stage completes, the script checks for HEALTH_OK.
+# a Ceph cluster (with various options to control the cluster configuration).
+# After the last stage completes, the script checks for HEALTH_OK.
 #
 # The script makes no assumptions beyond those listed in qa/README.
 #
-# On success (HEALTH_OK is reached), the script returns 0. On failure, for
-# whatever reason, the script returns non-zero.
+# After HEALTH_OK is reached, the script also runs various sanity tests
+# depending on the options provided.
+#
+# On success (HEALTH_OK is reached, sanity tests pass), the script returns 0.
+# On failure, for whatever reason, the script returns non-zero.
 #
 # The script produces verbose output on stdout, which can be captured for later
 # forensic analysis.
