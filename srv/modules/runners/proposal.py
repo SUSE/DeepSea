3bfb824f05c2bf82eb9d9e68e4861accb9666417
Joshua Schmid <jschmid@suse.de>
2019-02-28 10:33:41 +0100

    Add deprecation warning to proposal runner

---
+++ srv/modules/runners/proposal.py
@@ -0,0 +1,19 @@
+"""
+Deprecated OSD proposal generator
+"""
+
+deprecation_message = """This runner is deprecated. Please use the new Drive
+Group approach. Information can be found here:
+https://github.com/SUSE/DeepSea/wiki/Drive-Groups"""
+
+
+def peek():
+    print(deprecation_message)
+
+
+def populate():
+    print(deprecation_message)
+
+
+def help():
+    print(deprecation_message)
