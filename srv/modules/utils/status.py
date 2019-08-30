5e7671451b288c6d616fd82333dfe851c44c62f3
Tim Serong <tserong@suse.com>
2019-08-30 16:52:07 +1000

    status runner: move version query to utils directory
    Signed-off-by: Tim Serong <tserong@suse.com>
    (cherry picked from commit 0bb94dc84127ad2ec22e4649ca1a84374400c6bc)

---
+++ srv/modules/utils/status.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# vim: ts=8 et sw=4 sts=4
+
+import salt.client
+
+
+def get_sys_versions(cluster_name='ceph'):
+    """
+    Query grains, run commands for current versions
+    """
+    local = salt.client.LocalClient()
+    search = "I@cluster:{}".format(cluster_name)
+    # grains might be inaccurate or not up to date because they are designed
+    # to hold static data about the minion. In case of an update though, the
+    # data will change.  grains are refreshed on reboot(restart of the service).
+    os_codename = local.cmd(search, 'grains.get', ['oscodename'], tgt_type="compound")
+    salt_version = local.cmd(search, 'grains.get', ['saltversion'], tgt_type="compound")
+    ceph_version = local.cmd(search, 'cmd.shell', ['ceph --version'], tgt_type="compound")
+    return os_codename, salt_version, ceph_version
