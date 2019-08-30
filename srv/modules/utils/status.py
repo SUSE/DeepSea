4c62fccf10a547315c745dda05ef50b1f5283dd1
Tim Serong <tserong@suse.com>
2019-08-30 17:13:08 +1000

    utils/status: handle down nodes gracefully
    When querying grains and running commands, any nodes that are down will
    result in a dict value of False, and a 15 second delay (which is horrible).
    This commit detects down nodes on the first grains.get, then subsequent
    queries exclude those nodes, and we return "Unknown (node down?)" instead
    of False.
    
    Signed-off-by: Tim Serong <tserong@suse.com>
    (cherry picked from commit a8034b5a304bc592046cbda802ab5e0e4aa08039)

--- srv/modules/utils/status.py
+++ srv/modules/utils/status.py
@@ -14,6 +14,22 @@ def get_sys_versions(cluster_name='ceph'):
     # to hold static data about the minion. In case of an update though, the
     # data will change.  grains are refreshed on reboot(restart of the service).
     os_codename = local.cmd(search, 'grains.get', ['oscodename'], tgt_type="compound")
+
+    # If any nodes are down, the value of each item in the dict will be False.
+    # We can use this to modify the search string for the subsequent two calls,
+    # which will save 30 seconds of execution time (15 seconds per call) if any
+    # nodes are down.  So if any nodes are down, this function will take about
+    # 15 seconds to return (because the first search included down nodes), rather
+    # than 45 seconds (which is a very long time to stare at a blinking cursor)
+    down_nodes = [node for node in os_codename if not os_codename[node]]
+    if down_nodes:
+        search += " and not ( {} )".format("or ".join(down_nodes))
+
     salt_version = local.cmd(search, 'grains.get', ['saltversion'], tgt_type="compound")
     ceph_version = local.cmd(search, 'cmd.shell', ['test -e /usr/bin/ceph && ceph --version || echo "Not installed"'], tgt_type="compound")
+
+    # Better to indicate the node is down or inaccessible than just the bool False
+    for node in down_nodes:
+        os_codename[node] = salt_version[node] = ceph_version[node] = "Unknown (node down?)"
+
     return os_codename, salt_version, ceph_version
